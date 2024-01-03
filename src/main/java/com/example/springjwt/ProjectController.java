0849ea817a442a0a6cbfe161de62dd38da398628
progman <progman36@gmail.com>
2024-01-03 14:50:14 +0200

    time update

--- src/main/java/com/example/springjwt/ProjectController.java
+++ src/main/java/com/example/springjwt/ProjectController.java
@@ -4,13 +4,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
 import java.util.List;
 
 @RestController
 public class ProjectController {
     private UserService userService = new UserService();
+
+    @GetMapping("/time")
+    public Instant test() {
+        return Instant.now();
+    }
 
     @GetMapping("/bestUsers")
     public List<User> getBestUser() {
